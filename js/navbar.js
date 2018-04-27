87a795d63a8327b2191595eabfd7cae2a0cff156
Duc laptop <antonio.tran.ngoc@gmail.com>
2018-04-27 11:03:25 +0700

    completed

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,47 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
         $('.navbar-container').addClass('shrink');
-    }
-    else{
+    } else {
         $('.navbar-container').removeClass('shrink');
     }
 })
+
+$(document).ready(function (){
+            $("#backHome").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div1").offset().top
+                }, 2000);
+            });
+        });
+
+$(document).ready(function (){
+            $("#backFeatures").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div2").offset().top
+                }, 2000);
+            });
+        });
+
+$(document).ready(function (){
+            $("#backDesign").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div3").offset().top
+                }, 2000);
+            });
+        });
+
+$(document).ready(function (){
+            $("#backReviews").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div4").offset().top
+                }, 2000);
+            });
+        });
+
+$(document).ready(function (){
+            $("#backDownload").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div5").offset().top
+                }, 2000);
+            });
+        });
